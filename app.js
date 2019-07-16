3fb02b917a21f051950c0f29a00b1628569c220d
Josh Ludahl <josh.ludahl@gmail.com>
2019-07-15 18:12:27 -0700

    links

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ const bodyParser = require('body-parser');
 const helmet = require('helmet');
 
 //  Parse form data
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
 
 //  Custom logging
 const logger = require('./utils/logger');
@@ -22,7 +24,10 @@ app.use(helmet());
 //  CORS
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
   if (req.method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
     return res.status(200).json({});
@@ -32,7 +37,12 @@ app.use((req, res, next) => {
 
 //  Database Connection
 //  Connect to MongoDB (You will need to provide a username, password, and url to the database.)
-const connectionURL = 'mongodb://' + process.env.MONGO_USER + ':' + process.env.MONGO_PW + process.env.MONGO_URI;
+const connectionURL =
+  'mongodb://' +
+  process.env.MONGO_USER +
+  ':' +
+  process.env.MONGO_PW +
+  process.env.MONGO_URI;
 mongoose.connect(connectionURL, {
   useNewUrlParser: true,
   useCreateIndex: true
@@ -42,7 +52,7 @@ mongoose.connection.on('error', error => console.log(error));
 mongoose.Promise = global.Promise;
 
 //  Static Files
-app.use('/static', express.static(path.join(__dirname, 'static')))
+app.use('/static', express.static(path.join(__dirname, 'static')));
 
 //  Controllers
 app.use(require('./api/controllers'));
