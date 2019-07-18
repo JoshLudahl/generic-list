af2ef2c83ddeaac2f9a8817cefa7a87e7d009f29
Josh Ludahl <josh.ludahl@gmail.com>
2019-07-17 18:25:49 -0700

    Refactored weather
    Changed the weather api call to async fetch call and updated packages.

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,20 @@
-//import * as myModule from '/modules/my-module.js';
 const express = require('express');
 const router = express.Router();
+const fetch = require('node-fetch');
 
-router.get('/', (req, res, next) => {
-  res.render('index', { page: 'home' });
+router.get('/', async (req, res, next) => {
+  async function getWeather() {
+    let weather = await fetch(
+      'https://api.openweathermap.org/data/2.5/weather?id=5746545&units=imperial&appid=e5b292ae2f9dae5f29e11499c2d82ece'
+    );
+
+    let weatherData = await weather.json();
+    return weatherData;
+  }
+
+  const data = await getWeather();
+
+  res.render('index', { page: 'home', weather: data });
 });
 
 router.get('/about', (req, res, next) => {
